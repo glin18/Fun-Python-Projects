2d7c0450723e768721e292317864555f0f20acb4
Gary Lin <garylin2018@gmail.com>
2022-05-20 21:36:31 -0700

    Writes to JSON file. Search to retrieve data from JSON file

--- password-manager/main.py
+++ password-manager/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
 
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
@@ -24,6 +26,20 @@ def generate_password():
     pyperclip.copy(password)
 
 
+def search_website():
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+            searched_website = website_entry.get()
+            email = data[searched_website]["email"]
+            password = data[searched_website]["password"]
+            messagebox.showinfo(title=searched_website, message=f"Email: {email}\nPassword:{password}")
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    except KeyError:
+        messagebox.showinfo(title="Error", message="No details for the website exists")
+
+
 def add_clicked():
     with open("passwords.txt", "a") as file:
         if len(website_entry.get()) == 0 or len(email_entry.get()) == 0 or len(password_entry.get()) == 0:
@@ -35,10 +51,27 @@ def add_clicked():
                                                                        f"\nPassword:{password_entry.get()}"
                                                                        f"\nIs it okay to save?")
             if is_ok:
-                file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
-                website_entry.delete(0, 'end')
-                email_entry.delete(0, 'end')
-                password_entry.delete(0, 'end')
+                new_data = {
+                    website_entry.get(): {
+                        "email": email_entry.get(),
+                        "password": password_entry.get()
+                    }
+                }
+                try:
+                    with open("data.json", "r") as data_file:
+                        data = json.load(data_file)
+                        data.update(new_data)
+                except FileNotFoundError:
+                    with open("data.json", "w") as data_file:
+                        json.dump(new_data, data_file, indent=4)
+                else:
+                    with open("data.json", "w") as data_file:
+                        json.dump(data, data_file, indent=4)
+                finally:
+                    website_entry.delete(0, 'end')
+                    email_entry.delete(0, 'end')
+                    password_entry.delete(0, 'end')
+                # file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
 
 
 window = Tk()
@@ -59,12 +92,12 @@ email_label.grid(column=0, row=2)
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
 email_entry = Entry(width=35)
-email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.grid(column=1, row=2, columnspan=2, stick="W")
 
 password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3, sticky="W")
@@ -74,5 +107,8 @@ add.grid(column=1, row=4, columnspan=2)
 
 generate = Button(text="Generate Password", command=generate_password)
 generate.grid(column=2, row=3)
+
+search = Button(text="Search", width=10, command=search_website)
+search.grid(column=2, row=1)
 
 window.mainloop()
